5e4b382331d07b9a6200dd1a1ff07c03b71c98b0
Nhuuu <nqtrinh87@gmail.com>
2019-02-21 18:18:41 -0800

    first commit

--- in_the_sound_server/models/index.js
+++
@@ -1,8 +0,0 @@
-// Mongoose require and connect
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sound',
-  { useNewUrlParser: true });
-
-// Deprecation warning: https://github.com/Automattic/mongoose/issues/6922
-mongoose.set('useCreateIndex', true);
